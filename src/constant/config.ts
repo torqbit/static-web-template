6bfc0a43b490650674d22711ad68ba40f4a48867
Shadab Alam <shadab19it@gmail.com>
2024-02-19 14:50:40 +0530

    added site url

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   title: 'Torqbit - Next.js + Tailwind CSS + TypeScript Starter',
   description: 'A Torqbit starter for Next.js, Tailwind CSS, and TypeScript',
-  url: '',
+  url: 'http://torqbit.com',
 };
