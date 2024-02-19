e34529398bbc5832362e10bc438d4164ed434b94
Shadab Alam <shadab19it@gmail.com>
2024-02-19 14:06:28 +0530

    added screen break points

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,23 @@ export default {
         shimmer: 'shimmer 1.3s linear infinite',
       },
     },
+
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1636px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 } satisfies Config;
